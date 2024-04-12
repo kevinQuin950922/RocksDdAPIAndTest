3977ced8fb448880eb92343d3257f1b9c1d56924
kevinQuin950922 <kevin.caicedo@quind.io>
2024-04-12 16:29:52 -0500

    configurando server

--- src/main/java/com/example/scheduled/conf/ScheduledTasks.java
+++ src/main/java/com/example/scheduled/conf/ScheduledTasks.java
@@ -1,6 +1,8 @@
 package com.example.scheduled.conf;
 
 import com.example.scheduled.repositorio.KeyVauleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,14 @@ public class ScheduledTasks {
     public ScheduledTasks(KeyVauleRepository<String, String> rocksDB) {
         this.rocksDB = rocksDB;
     }
-
+    @Autowired
+    private Environment env;
 
     @Scheduled(cron = "${cron.expresion.tiempoScheduled}") // Ejecutar cada minuto
     public void ejecutarTareaProgramada() {
         // Agrega aquí la lógica de tu tarea programada
         System.out.println("Tarea programada ejecutada.");
+        System.out.println(env.getProperty("cron.expresion.tiempoScheduled"));
         rocksDB.delete("1");
     }
 }
